4830f277f6cf478e8d91665e4aa0a14aac765e48
arora767287 <57432791+arora767287@users.noreply.github.com>
2021-02-16 17:23:34 +0800

    Update controls_new.py

--- controls_new.py
+++ controls_new.py
@@ -12,17 +12,17 @@ while running:
        if event.type == pygame.QUIT:
           running = False
        if event.type == pygame.KEYUP:
-          ser.write(1)
-          ser.write(500)
+          ser.write("1")
+          ser.write("500")
        if event.type == pygame.KEYDOWN:
-          ser.write(2)
-          ser.write(500)
+          ser.write("2")
+          ser.write("500")
        if event.type == pygame.K_LEFT:
-          ser.write(3)
-          ser.write(500)
+          ser.write("3")
+          ser.write("500")
        if event.type == pygame.K_RIGHT:
-          ser.write(4)
-          ser.write(100)
-       if event.type == pygame. K_w:
-          ser.write(5)
-          ser.write(100)
+          ser.write("4")
+          ser.write("100")
+       if event.type == pygame.K_w:
+          ser.write("5")
+          ser.write("100")
