b3b9f0894e7bd20889dc1bc7fee24058d9453956
arora767287 <57432791+arora767287@users.noreply.github.com>
2021-03-20 21:49:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import pygame
+import serial
+import sympy
+import numpy
+import matplotlib
+import PySimpleGUI as sg
+import time
 def gstreamer_pipeline(sensor_id = 0,sensor_mode = 3,capture_width = 1280,capture_height = 1280,display_width = 1280,display_height = 1280,framerate = 60,flip_method = 0):
     return("nvarguscamerasrc sensor-id=%d sensor-mode=%d ! "
     "video/x-raw(memory:NVMM), "
