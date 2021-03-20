8feb0b276f2f72ee50488ebad91e55d1d7c2d69b
arora767287 <57432791+arora767287@users.noreply.github.com>
2021-03-20 23:55:11 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -39,5 +39,3 @@ class Interface():
             frame_grayversion = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             images_bytess = cv2.imencode(".png", frame_grayversion)[1].tobytes()
             window["camera"].update(data=images_bytess)
-    thread = Thread(target=update_interface)
-    thread.start()
