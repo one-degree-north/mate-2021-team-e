3f6be074bf95ddc844c2b2577eb2a0a6716a3a38
arora767287 <57432791+arora767287@users.noreply.github.com>
2021-03-20 23:04:46 +0800

    Update camera.py

--- camera.py
+++ camera.py
@@ -6,6 +6,7 @@ import matplotlib
 import PySimpleGUI as sg
 import time
 class Camera:
+    
     def __init__(self):
         #videos_information will contain the cv2 video capture object
         self.videos_information = None
@@ -19,15 +20,17 @@ class Camera:
         self.read_lock = None
         #whether the program is still running or not
         self.running = True
+        
     #This opens up the camera stream to allow frames to be analyzed through gstreamer
     def open(self, gstreamer_pipeline_string):
         try:
-            self.video_capture = cv.VideoCapture(gstreamer_pipeline_string, cv2.CAP_GSTREAMER)
+            self.video_capture = cv2.VideoCapture(gstreamer_pipeline_string, cv2.CAP_GSTREAMER)
         except RuntimeError:
             self.video_capture = None
             print("Camera is not working at this point in time on the robot")
             
             self.grabbed, self.frame = self.video_capture.read()
+            
     #This starts the camera stream and starts the thread that the camera will be streamed on        
     def start(self):
         if self.running:
@@ -38,6 +41,7 @@ class Camera:
             self.read_thread = threading.Thread(target = self.updateCamera)
             self.read_thread.start()
         return self  
+    
     #This updates the frames as they are inputted through the camera one by one          
     def updateCamera(self):
         while self.running:
@@ -48,16 +52,18 @@ class Camera:
                     self.frame = frame
             except RuntimeError:
                 print("Camera images are in the display but not on the actual interface")
+                
     #This reads the actual frames put through by the video capture object and creates copies of the frames while locking the thread that the camera output is being shown on            
     def read(self):
         with self.read_lock:
             frame = self.frame.copy()
             grabbed = self.grabbed
             return frame, grabbed
+        
     #This ends the camera stream and lets the camera stop outputting frames by setting the video capture object to none        
-     def release(self):
-         if self.video_capture!=None:
-             self.video_capture.release()
-             self.video_capture = None
-         if self.read_thread != None:
-             self.read_thread.join()     
+    def release(self):
+        if self.video_capture != None:
+            self.video_capture.release()
+            self.video_capture = None
+        if self.read_thread != None:
+            self.read_thread.join()     
