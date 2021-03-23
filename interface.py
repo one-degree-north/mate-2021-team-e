5030bcd6725abd61f6cb98ad4267acbcbe630745
arora767287 <57432791+arora767287@users.noreply.github.com>
2021-03-23 15:09:29 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -6,14 +6,22 @@ import matplotlib
 import PySimpleGUI as sg
 import time
 from threading import Thread
+import matplotlib.animation as animation
 
 class Interface():
     
     def __init__(self, robot, object_seen, motor_controls):
         
-        self.robot = None
-        self.object_seen = True
-        self.motor_controls = True
+        self.robot = robot
+        self.object_seen = object_seen
+        self.motor_controls = motor_controls
+        self.graph_angular_position = plt.figure()
+        self.graph_speed = plt.figure()
+        self.graph_position = plt.figure()
+        self.graph_angular_velocity = plt.figure()
+        self.graph_acceleration = plt.figure()
+        self.graph_power = plt.figure()
+        self.graphs = [self.angular_position, self.graph_speed, self.graph_position, self.graph_angular_velocity, self.graph_acceleration, self.graph_power]
     
     #make_interface makes the user interface that will use information parsed through from the camera 
     #and the controls in order to make information about the robot's speed, acceleration, and other measurements available
@@ -23,12 +31,22 @@ class Interface():
         self.speed_graph_layout = [[sg.Text("Speeds Graphs", size=(60,60), justification="center")], [sg.Image(filename="graph.png", key="graphs")]]
         self.layout = [camera_layout, speed_graph_layout]
         self.window = sg.Window(title="Interface", layout=layout, return_keyboard_events=True,location=(600,600))
+    def graph_update(self, time, data, subplot):
+        subplot.clear()
+        subplot.plot(time,data)
+    
         
     #update_interface puts the incoming pictures on the interface to show video output    
     def update_interface(self, cameraFrameSize, graphFrameSize, videoFrames):
         self.running = True
         self.cameraFrameSize = cameraFrameSize
         self.graphFrameSize = graphFrameSize
+        self.angular_position = angular_position
+        self.velocity = velocity
+        self.acceleration = acceleration
+        self.angular_velocity = angular_velocity
+        self.power = power
+        self.information = interfaceInformation()
         
         while self.running:
             event, values = window.read()
@@ -39,3 +57,9 @@ class Interface():
             frame_grayversion = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             images_bytess = cv2.imencode(".png", frame_grayversion)[1].tobytes()
             window["camera"].update(data=images_bytess)
+            for i in range(0, len(graphs)):
+                ani = animation.FuncAnimation(self.graphs[i], graph_update, interval=10000)
+            plt.show()    
+                #How to continuously show the graphs
+            
+            
