6303e5202310355bce5ef6b25149808163ece231
arora767287 <57432791+arora767287@users.noreply.github.com>
2021-03-30 16:31:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,8 +35,9 @@ if __name__ == '__main__':
     camera = Camera()
     camera.open(gstreamer_pipeline(sensor_id=0, sensor_mode=3, flip_method=0, display_height=600, display_width=600))
     camera.start()
-    controller = Control(100,0.1)
-    interface = Interface(robot=controller.list_movements, object_seen=True, motor_controls=True, clock=controller.clock)
+    clocks = pygame.time.Clock()
+    controller = Control(100,0.1,clocks)
+    interface = Interface(robot=controller.list_movements, object_seen=True, motor_controls=True, clock=clocks)
     interface.make_interface() 
     try:
         controller.controls_move()
