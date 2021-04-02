6b3e65ddf68a010eb19554cb0e97bee9570e4f44
arora767287 <57432791+arora767287@users.noreply.github.com>
2021-04-02 22:35:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ import matplotlib
 import PySimpleGUI as sg
 from threading import Thread
 import time
+import sys
 from camera import Camera
 from interface import Interface
 from controls import Control
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
 
 
 def gstreamer_pipeline(sensor_id = 0,sensor_mode = 3,capture_width = 1280,capture_height = 1280,display_width = 1280,display_height = 1280,framerate = 60,flip_method = 0):
@@ -32,33 +35,32 @@ def gstreamer_pipeline(sensor_id = 0,sensor_mode = 3,capture_width = 1280,captur
     ))
 
 if __name__ == '__main__':
+    
     camera = Camera()
     camera.open(gstreamer_pipeline(sensor_id=0, sensor_mode=3, flip_method=0, display_height=600, display_width=600))
     camera.start()
     clocks = pygame.time.Clock()
     controller = Control(100,0.1,clocks)
-    interface = Interface(robot=controller.list_movements, object_seen=True, motor_controls=True, clock=clocks)
-    interface.make_interface() 
+    #interface = Interface(robot=controller.list_movements, object_seen=True, motor_controls=True, clock=clocks)
+    #interface.make_interface() 
     try:
         controller.controls_move()
         print("Controller is working")
     except KeyboardInterrupt:
         print("Stop")
     except:
-        print("Unexpected error: ", sys.exc_infor()[0])
+        print("Unexpected error")
         raise
     if not camera.video_capture.isOpened():
         print("Start Cameras")
         SystemExit(0)
     thread = Thread(target=interface.update_interface, args=((600,600),camera_pictures))
     thread.start()
-    while self.running:
+    while controller.running:
         #Put interface update here
         _, camera_picture = camera.read()
         camera_pictures = np.hstack((camera_picture))
         camera.stop()
         camera.release()
+        print("Hello")
     thread.join()
-    
-    
-    
