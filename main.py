a9ab0e9d6e6f1bc631fa590aa592273ea57985f0
Lucas Sta Maria <lucas.stamaria@gmail.com>
2021-04-19 15:09:46 +0800

    feat: add claw use
    Signed-off-by: Lucas Sta Maria <lucas.stamaria@gmail.com>

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ DIMENSIONS: (int, int) = (800, 600)
 BACKGROUND: (int, int, int) = (0, 0, 0)
 
 pygame.init()
+pygame.joystick.init()
 
 screen = pygame.display.set_mode(DIMENSIONS)
 screen.fill(BACKGROUND)
 
 active = True
 
-PORT_NAME = "/dev/ttyUSB1"
-BAUD_RATE = 9600
+PORT_NAME = "/dev/ttyUSB0"
+BAUD_RATE = 115200
+
+controller = pygame.joystick.Joystick(0)
 
 ser = serial.Serial(port=PORT_NAME, baudrate=BAUD_RATE)
 ser.close()
@@ -29,7 +32,6 @@ t = threading.Thread(target=read_serial, args=(ser))
 t.start()
 
 def write_msg(msg):
-    pass
     ser.write(msg.encode("latin"))
 
 def move_forward(val):
@@ -65,6 +67,14 @@ def left_push(val):
     print(msg, end="")
     write_msg(msg)
 
+def claw_use(val):
+    msg = "4\n" + str(val) + "\n"
+    print(msg, end="")
+    write_msg(msg)
+
+def transform_val(val):
+    return int(300*val)
+
 while active:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -82,6 +92,8 @@ while active:
                 turn_right(300)
             if event.key == pygame.K_d:
                 turn_left(300)
+            if event.key == pygame.K_UP:
+                claw_use(180)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_w:
                 move_forward(0)
@@ -95,6 +107,27 @@ while active:
                 turn_right(0)
             if event.key == pygame.K_d:
                 turn_left(0)
+            if event.key == pygame.K_UP:
+                claw_use(0)
+        if event.type == pygame.JOYAXISMOTION:
+            # print(event)
+            if event.axis == 1:
+                move_up(transform_val(event.value))
+            if event.axis == 2:
+                if event.value > 0:
+                    turn_left(transform_val(event.value))
+                if event.value < 0:
+                    turn_right(transform_val(event.value))
+            if event.axis == 3:
+                move_forward(-1 * transform_val(event.value))
+        if event.type == pygame.JOYBUTTONDOWN:
+            if event.button == 7:
+                claw_use(0)
+            if event.button == 6:
+                claw_use(180)
+#         if event.type == pygame.JOYBUTTONUP:
+#             if event.button == 7:
+#                 claw_use(180)
 
 t.join()
 ser.close()
